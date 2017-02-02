c9b1706fc0f1c7bf58256361d5eee973174df3ac
Daniel Walton <dwalton76@gmail.com>
2017-02-02 07:50:17 -0500

    Renamed resolver.py rubiks-color-resolver.py

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setup(
     name='rubikscolorresolver',
     version='1.0.0',
     description='Resolve rubiks cube RGB values to the six cube colors',
-    keywords='rubik cube color',
+    keywords='rubiks cube color',
     url='https://github.com/dwalton76/rubiks-color-resolver',
     author='dwalton76',
     author_email='dwalton76@gmail.com',
     license='GPLv3',
+    scripts=['usr/bin/rubiks-color-resolver.py'],
     packages=['rubikscolorresolver'],
 )
